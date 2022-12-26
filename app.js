6d0b8029b3309016996bb40e9111af4d851a5213
Mariegolepas <97367355+Mariegolepas@users.noreply.github.com>
2022-12-26 15:56:18 +0100

    add userauth and productadd

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://adminuser:736HzJemjj2NbV@cluster0.jmmxck8.mongodb.net/go-fullstack?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+mongoose.set('strictQuery', true);
+
+const app = express();
+app.use(express.json()); //accès au corps de la requête
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
